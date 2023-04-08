d032d353c5dbd7e0eaf11b96fd322465c75a919a
Epicurius <niklas0702@gmail.com>
2023-04-08 12:38:30 +0300

    ft_malloc: use '_malloc()' and add exit message
    Re-routing allocation through '_malloc()', help keep the code simple and
    short.
    
    Signed-off-by: Epicurius <niklas0702@gmail.com>

--- srcs/ft_malloc.c
+++ srcs/ft_malloc.c
@@ -3,7 +3,7 @@
  * vim: ts=4 sw=4 tw=80 et ai si
  *
  * Created: 16/08/2021 Niklas Neronin
- * Updated: 08/04/2023 Niklas Neronin
+ * Updated: 10/04/2023 Niklas Neronin
  */
 
 #include "libdm.h"
@@ -24,22 +24,20 @@ void	*ft_malloc(size_t size, char *hash)
 {
 	void	*mem;
 
-	pthread_mutex_lock(&g_alloc.mutex);
 	if (size <= 0)
-		mem = NULL;
-	else if (size <= MEM_TINY_MAX)
-		mem = alloc_amount(MEM_TINY, MEM_TINY_ZONE, size);
-	else if (size <= MEM_SMALL_MAX)
-		mem = alloc_amount(MEM_SMALL, MEM_SMALL_ZONE, size);
-	else
-		mem = alloc_amount(MEM_LARGE, sizeof(t_block)
-				+ sizeof(t_zone) + size, size);
-	if (!mem)
-		exit(1);
+		return NULL;
 
-	if (hash)
-		copy_malloc_hash((void *)mem - sizeof(t_block), hash);
-	memset(mem, 0, size);
+	pthread_mutex_lock(&g_alloc.mutex);
+	mem = _malloc(size);
+	if (mem) {
+		memset(mem, 0, size);
+
+		if (hash)
+			copy_malloc_hash((void *)mem - sizeof(t_block), hash);
+	}
 	pthread_mutex_unlock(&g_alloc.mutex);
+
+	if (!mem)
+		ERROR("'ft_malloc()' failed allocating memory");
 	return mem;
 }
